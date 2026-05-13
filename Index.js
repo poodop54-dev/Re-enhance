66a36064775aed358c3811a8687ff08d1f2e3d53
poodop54-dev <poodop54@gmail.com>
2026-05-13 19:44:10 +0100

    Increase loop iteration limit in Index.js

--- Index.js
+++ Index.js
@@ -1,5 +1,5 @@
 javascript:(function(){
-    for(let i=0;i<999;i++){
+    for(let i=0;i<99999;i++){
         const d=document.createElement('div');
         d.style.cssText='position:fixed;top:0;left:0;width:100%;height:100%;backdrop-filter:blur(1px);pointer-events:none;opacity:0.01';
         document.body.appendChild(d);
